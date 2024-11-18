e7421df9a869f7f42d9108eeec46ac8053d03513
meawx <81612763+oiroe@users.noreply.github.com>
2024-11-18 15:00:21 +0000

    Added the test case and fix some bugs

--- calculator.py
+++ calculator.py
@@ -3,24 +3,63 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b #change b - a to a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+
+        if ((a < 0 and b < 0) or b < 0):   # both number are negative or b is negative
+            a = -a
+            b = -b
+
+        for i in range(b): #decrease 1 iteration
             result = self.add(result, a)
+
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        flag = 1
+
+        if (b == 0): #can't divide by zero
+            return None
+        if (a < 0 or b < 0): #check if either a or b are zero and change them in to positive also set flag to change result from positive to negative
+            flag = -1
+            if (a < 0):
+                a = -a
+            elif (b < 0):
+                b = -b
+
+        while a >= b: #change a > b to a >= b because it will complete the divide iteration
             a = self.subtract(a, b)
             result += 1
+
+        if (flag == -1):
+            result = -result
+
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        if (b == 0):
+            return None
+        
+        if ((a < 0 or a >= b) and b < 0):
+            b = -b
+            if (a < 0):
+                a = -a
+                while (a >= b):
+                    a -= b 
+            elif (a >= b):
+                while (a > 0):
+                    a -= b
+        elif ((a >= b or a < 0) and b >= 0):
+            if (a < 0):
+                while (a < 0):
+                    a += b
+            elif (a >= b):
+                while (a >= b):
+                    a -= b 
+
         return a
 
 # Example usage:
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,23 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(0, -1), -1)
+    
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(-1, 4), -5)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(-9, -2), 18)
+        self.assertEqual(self.calc.multiply(4, -2), -8)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(5, 1), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(5, 4), 1)
+        self.assertEqual(self.calc.modulo(-1, 5), 4)
 
     # Add the following test methods to the TestCalculator class:
 
